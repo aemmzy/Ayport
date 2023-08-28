8f33334feb83a9a0a9d06b1a1a65706022a39240
Ayomide Olagunju <76573702+aemmzy@users.noreply.github.com>
2023-08-28 11:57:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let sections =document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('section ul a');
+
+
+window.onscroll = ()=>{
+    sections.forEach(sec =>{
+        let top =window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height =sec.offsetHeight;
+        let id=sec.getAttribute("id");
+
+        if(top>= offset && top <offset + height){
+            navLinks.forEach(links=>{
+                links.classList.remove(active);
+                document.querySelector("section ul a[href'=' + id + ']").classList.add('active');
+            })
+        }
+    });
+}
+
+let menuIcon=document.querySelector('#menu-icon')
+let navbar=document.querySelector('#menu-content')
+
+menuIcon.onclick=() => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('show-menu');
+}
